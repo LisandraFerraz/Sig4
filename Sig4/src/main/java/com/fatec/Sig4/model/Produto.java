9abf1868887042fad53962651ed17acde69635ad
renanfigueoli09 <renanfigueoli@outlook.com>
2022-05-06 20:22:25 -0300

    adição do requerid no cadastraProdutor

--- Sig4/src/main/java/com/fatec/Sig4/model/Produto.java
+++ Sig4/src/main/java/com/fatec/Sig4/model/Produto.java
@@ -10,7 +10,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.joda.time.DateTime;
@@ -24,8 +27,13 @@ public class Produto {
 	private Long id;
 	@NotBlank(message = "Nome é requerido")
 	private String nome;
+	@Min(value=1, message = "Valor Minimo para estoque é 1.")
+	@Max(value=1000,message = "Valor Máximo para estoque é 1000.")
 	private int qtdEstoque;
+	@NotNull(message = "Preço é requerido")
 	private Double preco;
+	@Pattern(regexp = "^(0?[1-9]|[12][0-9]|3[01])[\\/-](0?[1-9]|1[012])[\\/-]\\d{4}$",
+			message = "A data de vencimento deve estar no formato dd/MM/YYYY")
 	private String dataCadastro;
 	
 	
