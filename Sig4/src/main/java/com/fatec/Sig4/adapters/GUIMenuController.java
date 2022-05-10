3d75af2258d91aa2c7bc5fcd534cd1aba6588d89
renanfigueoli09 <renanfigueoli@outlook.com>
2022-05-10 15:12:00 -0300

    coemçando a implementar o modo escuro nas demais páginas

--- Sig4/src/main/java/com/fatec/Sig4/adapters/GUIMenuController.java
+++ Sig4/src/main/java/com/fatec/Sig4/adapters/GUIMenuController.java
@@ -10,30 +10,51 @@ public class GUIMenuController {
 	public ModelAndView autenticacao() {
 		return new ModelAndView("paginaLogin");
 	}
-	
+	//Contraste
 	@GetMapping("/loginContraste")
 	public ModelAndView autenticacaoContraste() {
-		return new ModelAndView("paginaLoginContraste");
+		return new ModelAndView("paginasContraste/paginaLoginContraste");
 	}
-
+//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/")
 	public ModelAndView home() {
 		return new ModelAndView("paginaMenu");
 	}
+	//Contraste
+	@GetMapping("/Contraste")
+	public ModelAndView homeContraste() {
+		return new ModelAndView("paginasContraste/paginaMenuContraste");
+	}
+//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/tenis")
 	public ModelAndView acessarTenis() {
 		return new ModelAndView("tenis");
 	}
-	
+	//Contraste
+	@GetMapping("/tenisContraste")
+	public ModelAndView acessarTenisContraste() {
+		return new ModelAndView("paginasContraste/tenisContraste");
+	}
+//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/equipamentos")
 	public ModelAndView acessarEquipamentos() {
 		return new ModelAndView("equipamentos");
 	}
+	//Contraste
+	@GetMapping("/equipamentosContraste")
+	public ModelAndView acessarEquipamentosContraste() {
+		return new ModelAndView("paginasContraste/equipamentosContraste");
+	}
+//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/acessibilidade")
 	public ModelAndView acessarAcessibilidade() {
 		return new ModelAndView("acessibilidade");
 	}
-
+	//Contraste
+	@GetMapping("/acessibilidadeContraste")
+	public ModelAndView acessarAcessibilidadeContraste() {
+		return new ModelAndView("paginasContraste/acessibilidadeContraste");
+	}
 	
 	
 
