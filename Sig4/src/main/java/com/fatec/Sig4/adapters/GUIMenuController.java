989022dbefe83fbb92469e1463b08bf893c8ef31
renanfigueoli <renanfigueoli@outlook.com>
2022-05-17 21:42:19 -0300

    excluindo metodos ja não mais utilizados

--- Sig4/src/main/java/com/fatec/Sig4/adapters/GUIMenuController.java
+++ Sig4/src/main/java/com/fatec/Sig4/adapters/GUIMenuController.java
@@ -10,60 +10,24 @@ public class GUIMenuController {
 	public ModelAndView autenticacao() {
 		return new ModelAndView("paginaLogin");
 	}
-	//Contraste
-	@GetMapping("/loginContraste")
-	public ModelAndView autenticacaoContraste() {
-		return new ModelAndView("paginasContraste/paginaLoginContraste");
-	}
-//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/")
 	public ModelAndView home() {
 		return new ModelAndView("paginaMenu");
 	}
-	//Contraste
-	@GetMapping("/Contraste")
-	public ModelAndView homeContraste() {
-		return new ModelAndView("paginasContraste/paginaMenuContraste");
-	}
-//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/tenis")
 	public ModelAndView acessarTenis() {
 		return new ModelAndView("tenis");
 	}
-	//Contraste
-	@GetMapping("/tenisContraste")
-	public ModelAndView acessarTenisContraste() {
-		return new ModelAndView("paginasContraste/tenisContraste");
-	}
-//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/equipamentos")
 	public ModelAndView acessarEquipamentos() {
 		return new ModelAndView("equipamentos");
 	}
-	//Contraste
-	@GetMapping("/equipamentosContraste")
-	public ModelAndView acessarEquipamentosContraste() {
-		return new ModelAndView("paginasContraste/equipamentosContraste");
-	}
-//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/acessibilidade")
 	public ModelAndView acessarAcessibilidade() {
 		return new ModelAndView("acessibilidade");
 	}
-	//Contraste
-	@GetMapping("/acessibilidadeContraste")
-	public ModelAndView acessarAcessibilidadeContraste() {
-		return new ModelAndView("paginasContraste/acessibilidadeContraste");
-	}
-//////////////////////////////////////////////////////////////////////////////////
 	@GetMapping("/carrinhoDeCompra")
 	public ModelAndView acessarCarrinho() {
 		return new ModelAndView("carrinhoDeCompra");
-	}
-	
-	//Contraste
-	@GetMapping("/carrinhoDeCompraContraste")
-	public ModelAndView acessarCarrinhoContraste() {
-		return new ModelAndView("paginasContraste/carrinhoDeCompraContraste");
 	}
 }
