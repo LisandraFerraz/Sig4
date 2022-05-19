ccbea2cd8e2a9b36aa503eb7333b4dc8a46f147a
renanfigueoli <renanfigueoli@outlook.com>
2022-05-19 19:42:01 -0300

    adicionanado javaScript do botao a telas min1000px

---
+++ Sig4/src/main/resources/static/js/botao.js
@@ -0,0 +1,9 @@
+const botaoMenu = document.querySelector('.cabecalho__munu');
+			const menu = document.querySelector('.menu-lateral');
+			const EsconderVideo = document.querySelector('.conteudo')
+			
+							botaoMenu.addEventListener('click', ()=>{
+											menu.classList.toggle('menu-lateral--ativo');
+											EsconderVideo.classList.toggle('esconder__videos')
+
+							});
