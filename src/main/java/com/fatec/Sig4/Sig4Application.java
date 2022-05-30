f8af4a850d5c0ef178d1b70a210d04542f2eac1f
renanfigueoli09 <renanfigueoli@outlook.com>
2022-05-30 10:57:18 -0300

    teste

--- src/main/java/com/fatec/Sig4/Sig4Application.java
+++ src/main/java/com/fatec/Sig4/Sig4Application.java
@@ -15,7 +15,6 @@ import com.fatec.Sig4.ports.MantemCliente;
 import com.fatec.Sig4.ports.MantemPedido;
 import com.fatec.Sig4.ports.PedidoProdutoRepository;
 
-
 @SpringBootApplication
 public class Sig4Application implements CommandLineRunner {
 	@Autowired
