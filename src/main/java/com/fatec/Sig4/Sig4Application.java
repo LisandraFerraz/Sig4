30a9f18aca5aaec6d1816aa206eaef89c670a406
renanfigueoli09 <renanfigueoli@outlook.com>
2022-06-01 15:21:49 -0300

    Definindo o banco de dados para o do heroku

--- src/main/java/com/fatec/Sig4/Sig4Application.java
+++ src/main/java/com/fatec/Sig4/Sig4Application.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sig4Application {
-	
 	public static void main(String[] args) {
 	SpringApplication.run(Sig4Application.class, args);
 	}
-	
-
    }
